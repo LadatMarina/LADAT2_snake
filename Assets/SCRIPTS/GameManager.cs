8d89a1976d2043f6aba578800405a6703326fa00
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2023-10-03 09:45:53 +0200

    PushClase2
    -Asignam un ordre correcte de execució de scripts perquè sa llibreria de sprites s'executi de lo primer
    -Cream un game object amb es seu sprite corresponent i es components corresponents per codi.

--- Assets/SCRIPTS/GameManager.cs
+++ Assets/SCRIPTS/GameManager.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        /* Cream un game object anomenat "Snake_Head", l'hem asignat
+         * a una variable d'aquest script ("snakeHeadGameObject")
+         * Li afegim sa variable spriteRenderer
+         * Li posam s'sprite de sa llibreria de sprites des cap de sa snake
+         */
+        GameObject snakeHeadGameObject = new GameObject("Snake_Head");
+        SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeSpriteRenderer.sprite = GameAssets.Instance.snakeHeadSprite;
     }
 }
